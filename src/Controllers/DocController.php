21173c92b5340c96067f7440bb7a8a7ce7640d55
BrooksYang <brooksyang@outlook.com>
2017-12-18 09:58:01 +0800

    支持在线ApacheBench服务器压力测试

--- src/Controllers/DocController.php
+++ src/Controllers/DocController.php
@@ -7,6 +7,7 @@ use BrooksYang\LaravelApiHelper\Traits\DocHelper;
 use BrooksYang\LaravelApiHelper\Traits\GuzzleHelper;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use File;
 
 class DocController extends Controller
 {
@@ -58,12 +59,98 @@ class DocController extends Controller
         $method = $request->input('methodForApiDoc');
         $baseUrl = config('api-helper.api_base_url') ?: str_replace($request->path(), '', $request->url());
         $url = $baseUrl . $request->input('uriForApiDoc');
-        $params = $request->except('_token', 'methodForApiDoc', 'uriForApiDoc', 'token');
+        $params = $request->except('_token', 'methodForApiDoc', 'uriForApiDoc', 'token', 'total_requests', 'concurrency');
         $token = $request->input('tokenForApiDoc');
         $request->session()->put('tokenForApiDoc', $token);
 
+        // 发送请求
         $data = $this->sendRequest($method, $url, $params);
 
-        return back()->with('params', json_encode($data))->withInput();
+        // 压力测试
+        $response = $this->serverTest($request, $params, $method, $url, $token);
+
+        return back()->with('params', json_encode($data))
+            ->with('response', $response)
+            ->withInput();
+    }
+
+    /**
+     * 服务器压力测试
+     *
+     * @param Request $request
+     * @param         $params
+     * @param         $method
+     * @param         $url
+     * @param         $token
+     * @return array
+     */
+    private function serverTest(Request $request, $params, $method, $url, $token)
+    {
+        // 判断是否填写了压力测试参数
+        if ($request->has('total_requests') && $request->has('concurrency')) {
+            $totalRequests = $request->input('total_requests') ?: 100;
+            $concurrency = $request->input('concurrency') ?: 10;
+
+            $token = $token ? "-H 'Authorization:Bearer $token' " : '';
+
+            // GET请求url
+            $url = $this->getFullUrl($url, $method, $params);
+
+            // POST请求参数
+            $postParam = $this->getPostParam($method, $params);
+
+            // 执行压力测试
+            $command = "ab -n $totalRequests -c $concurrency {$postParam}{$token}$url";
+            exec($command, $report);
+
+            // 判断是否返回测试结果
+            $errorMessage = "压力测试模块（ApacheBench）未安装，请执行以下命令安装：\n\napt-get install apache2-utils";
+            $report = $report ? implode("\n", $report) : $errorMessage;
+        }
+
+        return compact('command', 'report', 'jsonParam');
+    }
+
+    /**
+     * 拼接GET方式url
+     *
+     * @param $url
+     * @param $method
+     * @param $params
+     * @return string
+     */
+    private function getFullUrl($url, $method, $params)
+    {
+        if ($method == 'GET') {
+            // 拼接参数
+            $getParam = '';
+            foreach ($params as $key => $param) {
+                $getParam .= "$key=$param&";
+            }
+            $url = $url . rtrim($getParam, '&');
+        }
+
+        return $url;
+    }
+
+    /**
+     * 获取POST请求参数
+     *
+     * @param $method
+     * @param $params
+     * @return string
+     */
+    private function getPostParam($method, $params)
+    {
+        $postParam = '';
+        if ($method == 'POST') {
+            unset($params['tokenForApiDoc']);
+            $jsonParam = json_encode($params);
+            $filename = storage_path('post_data.txt');
+            File::put($filename, $jsonParam);
+            $postParam = "-p $filename -T 'application/json' ";
+        }
+
+        return $postParam;
     }
 }
--- src/views/show.blade.php
+++ src/views/show.blade.php
@@ -26,6 +26,7 @@
         <div class="">
             <a href="{{ url("api/docs/$module") }}" class="button button-custom">接口列表</a>
             <button class="button is-outlined is-primary button-custom" type="submit">测试</button>
+            <button class="button is-outlined is-danger button-custom" type="submit" onclick="serverTest()">压测</button>
         </div>
 
         <div class="box">
@@ -52,11 +53,11 @@
             <hr>
             <div class="field has-addons">
                 <p class="control">
-                    <div class="select">
-                        <select>
-                            <option>Authorization: Bearer</option>
-                        </select>
-                    </div>
+                <div class="select">
+                    <select>
+                        <option>Authorization: Bearer</option>
+                    </select>
+                </div>
                 </p>
                 <p class="control is-expanded">
                     <input class="input" type="text" name="tokenForApiDoc" value="{{ session('tokenForApiDoc') }}" placeholder="请输入token">
@@ -91,19 +92,60 @@
             </div>
         @endif
 
+        {{-- 压力测试 --}}
+        <div class="box">
+            <strong>压力测试：</strong>
+            <hr>
+            <table class="table is-fullwidth is-narrow">
+                <tbody>
+                <tr>
+                    <td>总请求数</td>
+                    <td>
+                        <input class="input" type="text" id="total_requests_input" value="{{ old('total_requests') }}" placeholder="总请求数，默认100">
+                    </td>
+                </tr>
+                <tr>
+                    <td>并发量</td>
+                    <td>
+                        <input class="input" type="text" id="concurrency_input" value="{{ old('concurrency') }}" placeholder="并发量，默认10">
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+        {{-- 返回结果 --}}
         <div class="box">
             <strong>返回结果：</strong>
             <hr>
             <pre><div id="code">{{ session('params') }}</div></pre>
         </div>
+
+        {{-- 压力测试结果 --}}
+        @if (session('response.command'))
+            <div class="box">
+                <strong>压测结果：</strong>
+                <hr>
+                <pre>{{ session('response.command') }}<hr>{{ session('response.report') }}</pre>
+            </div>
+        @endif
     </form>
 @endsection
 
 @section('Js')
     <script>
+        // 格式化json
         if (window["JSON"] && JSON["stringify"]) {
             var code = document.getElementById("code");
             code.innerHTML = JSON.stringify(JSON.parse(code.innerHTML), undefined, 2);
+        }
+
+        /**
+         * 压力测试
+         */
+        function serverTest() {
+            document.getElementById('total_requests_input').setAttribute('name', 'total_requests');
+            document.getElementById('concurrency_input').setAttribute('name', 'concurrency');
         }
     </script>
 @endsection
