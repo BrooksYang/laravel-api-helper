aefa77cba2c4209b99f9eee1be5bf2b75ab9683d
Brooks <brooksyang@outlook.com>
2018-07-25 10:10:24 +0800

    test

---
+++ tests/ApiHelperTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ApiHelperTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testApiHelper()
+    {
+        $this->assertTrue(true);
+    }
+}
